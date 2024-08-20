2d4590667631b7e49d185f3cd69219f709990071
AdmiN <AdmiN@ITWS037>
2024-08-20 11:02:30 +0530

    adds login authentication and signup

--- src/Login.java
+++ src/Login.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String u_name = "vyasmanimala44@gmail.com";
+		String pwd = "Vyas@2003";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter username");
+		String ip1 = sc.nextLine();
+		System.out.println("enter password");
+		String ip2 = sc.nextLine();
+		if(u_name.equals(ip1) && pwd.equals(ip2))
+		{
+			System.out.println("Login successfull");
+		}
+		else {
+			System.out.println("Login Failed");
+		}
 	}
 
 }
--- src/Signup.java
+++ src/Signup.java
@@ -1,9 +1,15 @@
-
+import java.util.*;
 public class Signup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter username");
+		String ip1 = sc.nextLine();
+		System.out.println("enter password");
+		String ip2 = sc.nextLine();
+		System.out.println("Username "+ip1);
+		System.out.println("Password "+ip2);
 	}
 
 }
